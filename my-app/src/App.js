4cc7c6726e4161bb3c9603a77fbdf44ac4b703cf
Mono <molomjamts21@gmail.com>
2024-06-17 17:56:27 +1000

    updated prettier

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
-import Header from './components/Header'
-import {Container} from "reactstrap";
+import Header from './components/Header';
+import { Container } from 'reactstrap';
+import { TypeAnimation } from 'react-type-animation';
 
 const App = () => {
-    return (
-        <MainAppStyle>
-            <Header/>
-            <Container>
-                <h4>Hello, I'm Mono. A Passionate Software Engineer.</h4>
-            </Container>
-        </MainAppStyle>
-    );
-}
+  return (
+    <MainAppStyle>
+      <Header />
+      <Container>
+        <div className="main-section">
+          <h4>Hello, I'm Mono. A Passionate Software Engineer.</h4>
+          <TypeAnimation
+            sequence={[
+              'Java and React/Node.js',
+              1000,
+              'Java and React/Node.js developer',
+              2000,
+              'Java and Typescript/Next.js developer',
+              2000,
+              'PERN Full-Stack Developer',
+              2000,
+            ]}
+            wrapper="span"
+            speed={50}
+            style={{ fontSize: '50px', display: 'inline-block' }}
+            repeat={Infinity}
+          />
+        </div>
+      </Container>
+    </MainAppStyle>
+  );
+};
 
 const MainAppStyle = styled.div`
+  position: relative;
+  background-color: #000;
+  color: #fff;
+  text-align: center;
+
+  h4 {
+    font-size: 18px;
+    font-weight: 400;
+  }
+  .main-section {
     position: relative;
-    background-color: #000;
-    color: #fff;
-    text-align: center;
-
-    h4 {
-        font-size: 16px;
-    }
-
-    .anim {
-        height: 100vh;
-        background-color: black;
-    }
+    top: 40vh;
+  }
 `;
 
 export default App;
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,30 +1,31 @@
 import React from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Header = () => {
-    return (
-        <HeaderStyle>
-                <div>Navigation</div>
-                <div>Social</div>
-        </HeaderStyle>
-    );
-}
+  return (
+    <HeaderStyle>
+      <div>Navigation</div>
+      <div>Social</div>
+    </HeaderStyle>
+  );
+};
 
 const HeaderStyle = styled.div`
-    position: fixed;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    top: 10%;
-    color: #fff;
-    gap: 0 20px;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 60vw; 
-    border: 1px solid #ffffff;
-    padding: 20px;
-    border-radius: 999px;
-    text-align: center;
+  z-index: 100;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 7%;
+  color: #fff;
+  gap: 0 20px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 40vw;
+  border: 1px solid #ffffff33;
+  padding: 20px;
+  border-radius: 999px;
+  text-align: center;
 `;
 
 export default Header;
